55d6655a5b738db09ed93e72c7779be07d19b4fd
Balo01 <kazeembalo@gmail.com>
2022-09-16 01:53:51 -0700

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - a function that checks for uppercase character
  * @c: input
- * Return: l if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper (int c)
 {
