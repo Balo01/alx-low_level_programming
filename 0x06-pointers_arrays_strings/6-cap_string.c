6ed59dbf3fc838c16b0e27417ce31ef45a995a32
Balo01 <kazeembalo@gmail.com>
2022-09-22 20:43:48 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
-/**
-* cap_string - function that capitalize first character of a word
-* @str: string to capitalize
-* Return: returns the capitalized string
-*/
-
-char *cap_string(char *str)
-{
-	int a, b;
-	
-	char str[13] = {' ', '\t', '\n', ',', ';', '.',
-		'!', '?', '"', '(', ')', '{', '}'};
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
-			s[a] -= 32;
-
-		for (b = 0; b < 13; b++)
-		{
-			if (s[a] == str[b])
-			{
-				if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
-				{
-					s[a + 1] -= 32;
-				}
-			}
-		}
+/**                                                                  
+* cap_string - function that capitalize first character of a word    
+* @str: string to capitalize                                         
+* Return: returns the capitalized string                             
+*/                                                                   
+                                                                     
+char *cap_string(char *s)                                            
+{                                                                    
+        int a, b;                                                    
+                                                                     
+        char spe[13] = {' ', '\t', '\n', ',', ';', '.',              
+                '!', '?', '"', '(', ')', '{', '}'};                  
+                                                                     
+        for (a = 0; s[a] != '\0'; a++)                               
+        {                                                            
+                if (a == 0 && s[a] >= 'a' && s[a] <= 'z')            
+                        s[a] -= 32;                                  
+                                                                     
+                for (b = 0; b < 13; b++)                             
+                {                                                    
+                        if (s[a] == spe[b])                          
+                        {                                            
+                                if (s[a + 1] >= 'a' && s[a + 1] <= 'z
+')                                                                   
+                                {                                    
+                                        s[a + 1] -= 32;              
+                                }                                    
+                        }                                            
+                }
 	}
+
 	return (s);
 }
-
