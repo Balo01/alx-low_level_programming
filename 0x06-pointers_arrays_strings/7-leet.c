d703282763c6d8550f439d63e1a61f42be664393
Balo01 <kazeembalo@gmail.com>
2022-09-22 20:55:17 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,46 +1,21 @@
 #include "main.h"
 
 /**
- * leet - leet encoder
- * @str: string to be encoded 
- * 
- * Return: address of the encoded string 
- */
-
-char *leet(char *str)
+* leet - Converts to leet format
+* @s: string to be formatted
+* Return: formatted string
+*/
+char *leet(char *s)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (str);
+char options[] = {65, 52, 69, 51, 79, 48, 84, 55, 76, 49};
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j += 2)
+{
+if (s[i] == options[j] || s[i] == options[j] + 32)
+s[i] = options[j + 1];
 }
-
-/**
- * transform - helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char x)
-{
-	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replacement = x;
-
-	while (i < 8)
-	{
-		if (x == mapping_low[i] || x == mapping_upper[i])
-		{
-			replacement = i + '0';
-			break;
-		}
-		i++;
-	}
-	return (replacement);
+}
+return (s);
 }
