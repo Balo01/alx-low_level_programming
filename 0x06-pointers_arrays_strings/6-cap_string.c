4687fd0f7f26954cf1c3739c346b8eed93a7ff80
Balo01 <kazeembalo@gmail.com>
2022-09-22 20:49:06 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,33 @@
 #include "main.h"
 
-/**                                                                  
-* cap_string - function that capitalize first character of a word    
-* @str: string to capitalize                                         
-* Return: returns the capitalized string                             
-*/                                                                   
-                                                                     
-char *cap_string(char *s)                                            
-{                                                                    
-        int a, b;                                                    
-                                                                     
-        char spe[13] = {' ', '\t', '\n', ',', ';', '.',              
-                '!', '?', '"', '(', ')', '{', '}'};                  
-                                                                     
-        for (a = 0; s[a] != '\0'; a++)                               
-        {                                                            
-                if (a == 0 && s[a] >= 'a' && s[a] <= 'z')            
-                        s[a] -= 32;                                  
-                                                                     
-                for (b = 0; b < 13; b++)                             
-                {                                                    
-                        if (s[a] == spe[b])                          
-                        {                                            
-                                if (s[a + 1] >= 'a' && s[a + 1] <= 'z
-')                                                                   
-                                {                                    
-                                        s[a + 1] -= 32;              
-                                }                                    
-                        }                                            
-                }
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
 
 	return (s);
