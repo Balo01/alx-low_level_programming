2a53b9f2be0253761e90fcd8544afedc0d2bfa16
Balo01 <kazeembalo@gmail.com>
2022-09-22 20:12:38 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,29 +8,24 @@
 
 char *cap_string(char *str)
 {
-	int index = 0;
-
-	while (str[++index])
-	{
-		while (!(str[index] >= 'a') && (str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[indec - 1] == '}')
-			str[index] -= 32;
-	}
-
-	return (str);
+int a, b;
+char str[13] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
+for (a = 0; s[a] != '\0'; a++)
+{
+if (a == 0 && s[a] >= 'a' && s[a] <= 'z')
+s[a] -= 32;
+for (b = 0; b < 13; b++)
+{
+if (s[a] == str[b])
+{
+if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
+{
+s[a + 1] -= 32;
+}
+}
+}
+}
+return (str);
 }
 
