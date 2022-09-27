cbbad56b174bbc46dc7d16f680b05ac60a0592ae
Balo01 <kazeembalo@gmail.com>
2022-09-26 18:39:00 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,21 @@
-_putchar(char c);
-*_memset(char *s, char b, unsigned int n);
-*_memcpy(char *dest, char *src, unsigned int n);
-*_strchr(char *s, char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/** 
+* void prototypes()
+* int prototypes()
+*/
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-*_strpbrk(char *s, char *accept);
-*_strstr(char *haystack, char *needle);
-print_chessboard(char (*a)[8]);
-print_diagsums(int *a, int size);
-set_string(char **s, char *to);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif /*MAIN_H*/
+
